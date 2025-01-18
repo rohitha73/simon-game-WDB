079e54fa970e3be2a5645176310f842d2eec33c8
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2025-01-19 02:10:10 +0530

    added starting function

--- index.js
+++ index.js
@@ -1,33 +1,52 @@
-
 var randomMath = Math.random();
 randomMath = randomMath*4;
 randomMath = Math.floor(randomMath) +1;
 
-switch(randomMath) {
-    case 1:
-        var audio = new Audio('./sounds/red.mp3');
-        audio.play();
-        console.log("1")
-        break;
-    case 2:
-        var audio = new Audio('./sounds/green.mp3');
-        audio.play();
-        console.log("2")
-        break;
-    case 3:
-        var audio = new Audio('./sounds/blue.mp3');
-        audio.play();
-        console.log("3")
-        break;
-    case 4:
-        var audio = new Audio('./sounds/yellow.mp3');
-        audio.play();
-        console.log("4")
-        break;
+$(document).ready(function () {
+    var startKey = "";
+    
+    $(document).on('keydown', function (event) {
+        startKey = event.key;
+        starting();
+    });
+});
+
+
+
+function starting(){
+    switch(randomMath) {
+        case 1:
+            var red = new Audio('/sounds/red.mp3');
+            red.play();
+            setTimeout(function(){ $('.red').addClass('pressed');},0); 
+            setTimeout(function(){ $('.red').removeClass('pressed');},200); 
+            break;
+        case 2:
+            var green = new Audio('/sounds/green.mp3');
+            green.play();
+            setTimeout(function(){ $('.green').addClass('pressed');},0); 
+            setTimeout(function(){ $('.green').removeClass('pressed');},200); 
+            break;
+        case 3:
+            var blue = new Audio('/sounds/blue.mp3');
+            blue.play();
+            setTimeout(function(){ $('.blue').addClass('pressed');},0); 
+            setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
+            break;
+        case 4:
+            var yellow = new Audio('/sounds/yellow.mp3');
+            yellow.play();
+            setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
+            setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
+            break;
+        default:
+            alert("Error");
+    }
 }
+    
 
 
-/*$('.btn').click(function(event) {
+$('.btn').click(function(event) {
     var buttonClass = event.target.classList[1];
 
     switch(buttonClass) {
@@ -60,4 +79,3 @@ switch(randomMath) {
     }
 });
 
-*/
