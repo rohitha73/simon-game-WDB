ea4b5740dca3cac7a3ee70265406b855f392240d
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2025-01-19 23:03:58 +0530

    updated the loop

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var randomMath = Math.random();
-randomMath = randomMath*4;
-randomMath = Math.floor(randomMath) +1;
 
 $(document).ready(function () {
     var startKey = "";
@@ -12,70 +9,88 @@ $(document).ready(function () {
 });
 
 
-
 function starting(){
-    switch(randomMath) {
-        case 1:
-            var red = new Audio('/sounds/red.mp3');
-            red.play();
-            setTimeout(function(){ $('.red').addClass('pressed');},0); 
-            setTimeout(function(){ $('.red').removeClass('pressed');},200); 
-            break;
-        case 2:
-            var green = new Audio('/sounds/green.mp3');
-            green.play();
-            setTimeout(function(){ $('.green').addClass('pressed');},0); 
-            setTimeout(function(){ $('.green').removeClass('pressed');},200); 
-            break;
-        case 3:
-            var blue = new Audio('/sounds/blue.mp3');
-            blue.play();
-            setTimeout(function(){ $('.blue').addClass('pressed');},0); 
-            setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
-            break;
-        case 4:
-            var yellow = new Audio('/sounds/yellow.mp3');
-            yellow.play();
-            setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
-            setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
-            break;
-        default:
-            alert("Error");
+
+    for(var i = 0; i <= 4; i++){
+        var randomMath = Math.random();
+        randomMath = randomMath * 4;
+        randomMath = Math.floor(randomMath) + 1;
+    
+        switch(randomMath) {
+            case 1:
+                setTimeout(function(){ $('.red').addClass('pressed');},300); 
+                var red = new Audio('/sounds/red.mp3');
+                setTimeout(() => {
+                    red.play();
+                }, 300);
+                setTimeout(function(){ $('.red').removeClass('pressed');},600); 
+                break;
+            case 2:
+                setTimeout(function(){ $('.green').addClass('pressed');},600); 
+                var green = new Audio('/sounds/green.mp3');
+                setTimeout(() => {
+                    green.play();
+                }, 600);
+                setTimeout(function(){ $('.green').removeClass('pressed');},1200); 
+                break;
+            case 3:
+                setTimeout(function(){ $('.blue').addClass('pressed');},1400); 
+                var blue = new Audio('/sounds/blue.mp3');
+                setTimeout(() => {
+                    blue.play();
+                }, 1400);
+                setTimeout(function(){ $('.blue').removeClass('pressed');},1700); 
+                break;
+            case 4:
+                setTimeout(function(){ $('.yellow').addClass('pressed');},2000); 
+                var yellow = new Audio('/sounds/yellow.mp3',1700);
+                setTimeout(() => {
+                    yellow.play();
+                }, 2000);
+                setTimeout(function(){ $('.yellow').removeClass('pressed');},2300); 
+                break;
+            default:
+                alert("Error");
+        }
+
+        
     }
+
+    $('.btn').click(function(event) {
+        var buttonClass = event.target.classList[1];
+    
+        switch(buttonClass) {
+            case 'red':
+                setTimeout(function(){ $('.red').addClass('pressed');},0); 
+                setTimeout(function(){ $('.red').removeClass('pressed');},200); 
+                var audio = new Audio('/sounds/red.mp3');
+                audio.play();
+                break;
+            case 'green':
+                setTimeout(function(){ $('.green').addClass('pressed');},0); 
+                setTimeout(function(){ $('.green').removeClass('pressed');},200); 
+                var audio = new Audio('/sounds/green.mp3');
+                audio.play();
+                break;
+            case 'blue':
+                setTimeout(function(){ $('.blue').addClass('pressed');},0); 
+                setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
+                var audio = new Audio('/sounds/blue.mp3');
+                audio.play();
+                break;
+            case 'yellow':
+                setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
+                setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
+                var audio = new Audio('/sounds/yellow.mp3');
+                audio.play();
+                break;
+            default:
+                break;
+        }
+    });
 }
     
 
 
-$('.btn').click(function(event) {
-    var buttonClass = event.target.classList[1];
 
-    switch(buttonClass) {
-        case 'red':
-            setTimeout(function(){ $('.red').addClass('pressed');},0); 
-            setTimeout(function(){ $('.red').removeClass('pressed');},200); 
-            var audio = new Audio('/sounds/red.mp3');
-            audio.play();
-            break;
-        case 'green':
-            setTimeout(function(){ $('.green').addClass('pressed');},0); 
-            setTimeout(function(){ $('.green').removeClass('pressed');},200); 
-            var audio = new Audio('/sounds/green.mp3');
-            audio.play();
-            break;
-        case 'blue':
-            setTimeout(function(){ $('.blue').addClass('pressed');},0); 
-            setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
-            var audio = new Audio('/sounds/blue.mp3');
-            audio.play();
-            break;
-        case 'yellow':
-            setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
-            setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
-            var audio = new Audio('/sounds/yellow.mp3');
-            audio.play();
-            break;
-        default:
-            break;
-    }
-});
 
