eac040b48ac0f7b67aaac772a06544539c78ec98
rohitha73 <90081694+rohitha73@users.noreply.github.com>
2025-01-20 01:51:47 +0530

    updated the pattern

--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 
 $(document).ready(function () {
-    var startKey = "";
     
     $(document).on('keydown', function (event) {
+        var startKey = "";
         startKey = event.key;
         starting();
     });
 });
 
+function getRandomMath() {
+    let randomMath = Math.random();
+    randomMath = randomMath * 4;
+    randomMath = Math.floor(randomMath) + 1;
+    return randomMath;
+}
+
 
 function starting(){
 
-    for(var i = 0; i <= 4; i++){
-        var randomMath = Math.random();
-        randomMath = randomMath * 4;
-        randomMath = Math.floor(randomMath) + 1;
-    
-        switch(randomMath) {
+    let thePattern = [];
+    console.log(thePattern);
+
+    for(var i = 0; i <= 3; i++){
+        getRandomMath();
+
+        switch(getRandomMath()) {
             case 1:
                 setTimeout(function(){ $('.red').addClass('pressed');},300); 
                 var red = new Audio('/sounds/red.mp3');
@@ -24,6 +32,8 @@ function starting(){
                     red.play();
                 }, 300);
                 setTimeout(function(){ $('.red').removeClass('pressed');},600); 
+                thePattern.push(getRandomMath());
+                console.log(thePattern);
                 break;
             case 2:
                 setTimeout(function(){ $('.green').addClass('pressed');},600); 
@@ -32,6 +42,8 @@ function starting(){
                     green.play();
                 }, 600);
                 setTimeout(function(){ $('.green').removeClass('pressed');},1200); 
+                thePattern.push(getRandomMath());
+                console.log(thePattern);
                 break;
             case 3:
                 setTimeout(function(){ $('.blue').addClass('pressed');},1400); 
@@ -40,6 +52,8 @@ function starting(){
                     blue.play();
                 }, 1400);
                 setTimeout(function(){ $('.blue').removeClass('pressed');},1700); 
+                thePattern.push(getRandomMath());
+                console.log(thePattern);
                 break;
             case 4:
                 setTimeout(function(){ $('.yellow').addClass('pressed');},2000); 
@@ -48,6 +62,8 @@ function starting(){
                     yellow.play();
                 }, 2000);
                 setTimeout(function(){ $('.yellow').removeClass('pressed');},2300); 
+                thePattern.push(getRandomMath());
+                console.log(thePattern);
                 break;
             default:
                 alert("Error");
@@ -61,29 +77,65 @@ function starting(){
     
         switch(buttonClass) {
             case 'red':
-                setTimeout(function(){ $('.red').addClass('pressed');},0); 
-                setTimeout(function(){ $('.red').removeClass('pressed');},200); 
-                var audio = new Audio('/sounds/red.mp3');
-                audio.play();
-                break;
+                if(thePattern[0] === 1){
+                    setTimeout(function(){ $('.red').addClass('pressed');},0); 
+                    setTimeout(function(){ $('.red').removeClass('pressed');},200); 
+                    var red = new Audio('/sounds/red.mp3');
+                    red.play();
+                    break;
+                }
+                else{
+                    var wrong = new Audio('/sounds/wrong.mp3');
+                    setTimeout(() => {
+                        wrong.play();
+                        $('h1').html('Game Over');
+                    }, 500);
+                }
             case 'green':
-                setTimeout(function(){ $('.green').addClass('pressed');},0); 
-                setTimeout(function(){ $('.green').removeClass('pressed');},200); 
-                var audio = new Audio('/sounds/green.mp3');
-                audio.play();
-                break;
+                if(thePattern[0] === 2){
+                    setTimeout(function(){ $('.green').addClass('pressed');},0); 
+                    setTimeout(function(){ $('.green').removeClass('pressed');},200); 
+                    var green = new Audio('/sounds/green.mp3');
+                    green.play();
+                    break;
+                }
+                else{
+                    var wrong = new Audio('/sounds/wrong.mp3');
+                    setTimeout(() => {
+                        wrong.play();
+                        $('h1').html('Game Over');
+                    }, 500);
+                }
             case 'blue':
-                setTimeout(function(){ $('.blue').addClass('pressed');},0); 
-                setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
-                var audio = new Audio('/sounds/blue.mp3');
-                audio.play();
-                break;
+                if(thePattern[0] === 3){
+                    setTimeout(function(){ $('.blue').addClass('pressed');},0); 
+                    setTimeout(function(){ $('.blue').removeClass('pressed');},200); 
+                    var blue = new Audio('/sounds/blue.mp3');
+                    blue.play();
+                    break;
+                }
+                else{
+                    var wrong = new Audio('/sounds/wrong.mp3');
+                    setTimeout(() => {
+                        wrong.play();
+                        $('h1').html('Game Over');
+                    }, 500);
+                }
             case 'yellow':
-                setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
-                setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
-                var audio = new Audio('/sounds/yellow.mp3');
-                audio.play();
-                break;
+                if(thePattern[0] === 4){
+                    setTimeout(function(){ $('.yellow').addClass('pressed');},0); 
+                    setTimeout(function(){ $('.yellow').removeClass('pressed');},200); 
+                    var yellow = new Audio('/sounds/yellow.mp3');
+                    yellow.play();
+                    break;
+                }
+                else{
+                    var wrong = new Audio('/sounds/wrong.mp3');
+                    setTimeout(() => {
+                        wrong.play();
+                        $('h1').html('Game Over');
+                    }, 500);
+                }
             default:
                 break;
         }
